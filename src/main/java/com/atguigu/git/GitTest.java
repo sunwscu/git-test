9fe193fd3f1a1f23e216be95916cf1cf0153fe6c
sunwscu <107940456+sunwscu@users.noreply.github.com>
2022-06-22 01:39:38 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hey");
         System.out.println("hot fix2");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
